ea0751047d496bd841bd65c957ea58b8643f4ca2
chaitu9669 <chaitanya_onweb@yahoo.co.in>
2018-12-23 21:22:27 -0500

    Initial Commit

---
+++ src/main/java/com/self/learning/fluffy_octo_computing_machine/App.java
@@ -0,0 +1,13 @@
+package com.self.learning.fluffy_octo_computing_machine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
